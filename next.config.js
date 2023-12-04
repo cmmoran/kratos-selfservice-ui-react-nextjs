2bbe0f4b3f9c6b9bacb0eee737b1d45211a4369f
Chris Moran <chris@chrismoran.com>
2023-12-04 06:27:16 -0500

    fix: make BASE_PATH truly optional

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  basePath: `${process.env.BASE_PATH}`,
+  basePath: `${process.env.BASE_PATH || ""}`,
   env: {
     COOKIE_DOMAIN: `${process.env.COOKIE_DOMAIN}`,
     THEME: `${process.env.THEME}`,
